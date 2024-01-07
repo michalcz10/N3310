9b400103647c696246fb3dff684f19a53630811e
Michal Sedlák <michalsedlak35@gmail.com>
2024-01-07 21:03:19 +0100

    Hodne moc zmen :3

--- My project/Assets/scripts/LoadScene.cs
+++ My project/Assets/scripts/LoadScene.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,14 +11,27 @@ public class LoadScene : MonoBehaviour
 
     public void LoadPong()
     {
+        Invoke("DelayPong", 0.5f);
+    }
+    private void DelayPong()
+    {
         SceneManager.LoadScene("Pong");
     }
 
     public void LoadMainMenu()
     {
+        Invoke("DelayLoadMainMenu", 0.5f);
+    }
+    private void DelayLoadMainMenu()
+    {
         SceneManager.LoadScene("mainmenu");
     }
+
     public void QuitApplication()
+    {
+        Invoke("DelayQuitApplication", 0.5f);
+    }
+    private void DelayQuitApplication()
     {
         Application.Quit();
     }
